49607eb92e1732d326a10295f6882262c8ca44fe
OlgaNikulina <nikulinaov@inbox.ru>
2020-02-14 14:26:57 +0500

    initial  commit

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("public class Main {\n" +
-                "  public static void main(String[] args) {\n" +
-                "    // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов\n" +
-                "    String number = \"5351719427810741\";\n" +
-                "    System.out.println(String.format(\"Result is %s\", isValidCardNumber(number) ? \"OK\" : \"FAIL\"));\n" +
-                "  }\n" +
-                "\n" +
-                "  public static boolean isValidCardNumber(String number) {\n" +
-                "    if (number == null) {\n" +
-                "      return false;\n" +
-                "    }\n" +
-                "\n" +
-                "    if (number.length() != 16) {\n" +
-                "      return false;\n" +
-                "    }\n" +
-                "\n" +
-                "    long result = 0;\n" +
-                "    for (int i = 0; i < number.length(); i++) {\n" +
-                "      int digit;\n" +
-                "      try {\n" +
-                "        digit = Integer.parseInt(number.charAt(i) + \"\");\n" +
-                "      } catch (NumberFormatException e) {\n" +
-                "        return false;\n" +
-                "      }\n" +
-                "\n" +
-                "      if (i % 2 == 0) {\n" +
-                "        digit *= 2;\n" +
-                "        if (digit > 9) {\n" +
-                "          digit -= 9;\n" +
-                "        }\n" +
-                "      }\n" +
-                "      result += digit;\n" +
-                "    }\n" +
-                "\n" +
-                "    return (result != 0) && (result % 10 == 0);\n" +
-                "  }\n" +
-                "}");
+        // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
+        String number = "7896542123625478";
+        System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
+    }
+
+    public static boolean isValidCardNumber(String number) {
+        if (number == null) {
+            return false;
+        }
+
+        if (number.length() != 16) {
+            return false;
+        }
+
+        long result = 0;
+        for (int i = 0; i < number.length(); i++) {
+            int digit;
+            try {
+                digit = Integer.parseInt(number.charAt(i) + "");
+            } catch (NumberFormatException e) {
+                return false;
+            }
+
+            if (i % 2 == 0) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit -= 9;
+                }
+            }
+            result += digit;
+        }
+
+        return (result != 0) && (result % 10 == 0);
     }
 }
+
+
